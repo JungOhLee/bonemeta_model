03d6afd95082ea7800a73f5186e4f535506a6c0c
JOLEE <leejung209@gmail.com>
2021-05-24 00:52:52 +0900

    augmentation function and data downloader from s3bucket was added

---
+++ augmentation.py
@@ -0,0 +1,76 @@
+# Augmentation
+import random
+import math
+import torch.nn.functional as F
+
+def get_transform(image, label, augmentation_dict={'flip': True, 'offset': 0.1, 'scale':0.2, 'rotate':True,'noise': 0.1}):
+    # image, label = image_label_tuple
+    image, label = torch.tensor(image,dtype=torch.float32), torch.tensor(label, dtype=torch.float32)
+    image = image.unsqueeze(0).unsqueeze(0)
+    label = label.unsqueeze(0).unsqueeze(0)
+
+    transform_t = torch.eye(4)
+
+    for i in range(3):
+        if 'flip' in augmentation_dict:
+            if random.random() > 0.5:
+                transform_t[i,i] *= -1
+
+        if 'offset' in augmentation_dict:
+            offset_float = augmentation_dict['offset']
+            random_float = (random.random() * 2 - 1)
+            transform_t[i,3] = offset_float * random_float
+
+        if 'scale' in augmentation_dict:
+            scale_float = augmentation_dict['scale']
+            random_float = (random.random() * 2 - 1)
+            transform_t[i,i] *= 1.0 + scale_float * random_float
+
+
+    if 'rotate' in augmentation_dict:
+        angle_rad = random.random() * math.pi * 2
+        s = math.sin(angle_rad)
+        c = math.cos(angle_rad)
+
+        rotation_t = torch.tensor([
+            [c, -s, 0, 0],
+            [s, c, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ])
+
+        transform_t @= rotation_t
+
+    affine_t = F.affine_grid(
+            transform_t[:3].unsqueeze(0).to(torch.float32),
+            image.size(),
+            align_corners=False,
+        )
+
+    affine_t_label = F.affine_grid(
+            transform_t[:3].unsqueeze(0).to(torch.float32),
+            label.size(),
+            align_corners=False,
+        )
+
+    augmented_chunk = F.grid_sample(
+            image,
+            affine_t,
+            padding_mode='border',
+            align_corners=False,
+        ).to('cpu')
+
+    augmented_label = F.grid_sample(
+            label,
+            affine_t_label,
+            padding_mode='border',
+            align_corners=False,
+        ).to('cpu')
+
+    if 'noise' in augmentation_dict:
+        noise_t = torch.randn_like(augmented_chunk)
+        noise_t *= augmentation_dict['noise']
+
+        augmented_chunk += noise_t
+
+    return augmented_chunk[0].squeeze(0), augmented_label[0].squeeze(0).to(torch.bool)
---
+++ downloader.py
@@ -0,0 +1,92 @@
+import numpy as np
+import os
+import s3fs
+import matplotlib.pyplot as plt
+import SimpleITK as sitk
+import random
+
+class DataDownloader():
+	def __init__(self, remote_dir, local_dir, img_type, label_type, API_key, API_secret, s3bucket):
+		self.remote_dir = remote_dir
+		self.local_dir = local_dir
+		self.img_type = img_type
+		self.label_type = label_type
+		self.API_key = API_key
+		self.API_secret = API_secret
+
+		self.fs = s3fs.S3FileSystem(key = self.API_key, secret = self.API_secret)
+		self.bucket = s3bucket
+		print(self.bucket, self.remote_dir, self.img_type)
+		self.remote_data_location = 's3://{0}'.format(os.path.join(self.bucket, self.remote_dir, self.img_type))
+
+	def set_local_dir(self):
+		self.set_dir(self.local_dir)
+
+		self.local_npz_dir = os.path.join(self.local_dir, 'npz')
+		self.train_download_npz_dir = os.path.join(self.local_npz_dir, 'train')
+		self.val_download_npz_dir = os.path.join(self.local_npz_dir, 'val')
+		self.set_dir(self.local_npz_dir)
+		self.set_dir(self.train_download_npz_dir)
+		self.set_dir(self.val_download_npz_dir)
+
+		self.target_dir = os.path.join(self.local_dir, self.img_type)
+		self.target_train_dir = os.path.join(self.target_dir, 'train')
+		self.target_val_dir = os.path.join(self.target_dir, 'val')
+		self.set_dir(self.target_dir)
+		self.set_dir(self.target_train_dir)
+		self.set_dir(self.target_val_dir)
+
+	def set_dir(self, path):
+		if not os.path.exists(path):
+		  os.mkdir(path)
+
+	def download_and_unzip(self):
+		filenames = [file['name'] for file in self.fs.listdir(self.remote_data_location)]
+
+		random.seed(3)
+		val_list = random.choices(filenames, k = len(filenames)//5 + 1 )
+		train_list = list(set(filenames) - set(val_list))
+
+		print("Downloading npz..")
+		self.download_npz(train_list, self.train_download_npz_dir)
+		self.download_npz(val_list, self.val_download_npz_dir)
+		print("Unzipping npz..")
+		self.unzip(train_list, self.train_download_npz_dir, self.target_train_dir)
+		self.unzip(val_list, self.val_download_npz_dir, self.target_val_dir)
+		print("Download and unzip completed")
+
+
+	def download_npz(self, file_list, download_dir):
+		for file in file_list: 
+		  self.fs.get(file, os.path.join(download_dir, os.path.split(file)[-1]))
+
+	def unzip(self, npz_file_list, npz_dir, target_dir):
+		for file_name in npz_file_list: 
+		  file_path = os.path.join(npz_dir, os.path.split(file_name)[-1])
+		  data_and_labels = np.load(file_path)
+		  datas = data_and_labels['data']
+		  labels = data_and_labels['original_label']
+		  for i in range(len(datas)):
+		    target_file_name = os.path.splitext(os.path.split(file_name)[-1])[0] + '_{:04d}'.format(i) + '.npy'
+		    target_file_path = os.path.join(target_dir, target_file_name)
+		    single_data_and_label = np.concatenate([datas[i,:], labels[i,:][np.newaxis, :]], axis=0)
+		    np.save(target_file_path, single_data_and_label)
+
+	def check_sanity(self):
+		download_files = self.fs.listdir(self.remote_data_location)
+		train_files = os.path.listdir(self.target_train_dir)
+		val_files = os.path.listdir(self.target_val_dir)
+
+		train_npz = set([filename[:-9] for filename in train_files])
+		val_npz = set([filename[:-9] for filename in val_files])
+		total_npz_num = len(train_npz) + len(val_npz)
+
+		print("Remote npz_files: {}, Downloaded npz files: {} \n Train npz num: {}, Val npz num: ".format(
+			len(download_files), total_npz_num, len(train_npz), len(val_npz)))
+		print("Train npy path: {}, Val npy path:{}".format(self.target_train_dir, self.target_val_dir))
+
+	def set_all(self):
+		self.set_local_dir()
+		self.download_and_unzip()
+		self.check_sanity()
+
